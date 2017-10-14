a143adf2359caf90ee7d2fc1a01c3856bf5c32f0
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-14 13:53:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season}: #{holiday}"
-
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
