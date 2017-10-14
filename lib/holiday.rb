57e9197fdc5befcb14107f1a4df4d8204a37f56b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-14 13:58:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |day, supplies|
-      puts " #{day}: #{supplies.join(", ")}"
+      puts " #{day}: #{supplies.capitalize.join(", ")}"
     end
   end
 end
